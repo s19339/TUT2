d1e794bd1eb473351c44fdc9ae9c8bcfbf3ec092
ASUS <ASUS@INTEL>
2020-03-22 19:10:46 +0100

    Finished

---
+++ Tut2/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Tut2.Models
+{
+    public class Student
+    {
+
+        [JsonPropertyName("indexNumber")]
+        [XmlAttribute(AttributeName = "indexNumber")]
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string EmailId { get; set; }
+
+        public string MotherName { get; set; }
+
+        public string FatherName { get; set; }
+
+        public Studies studies { get; set; }
+
+
+    }
+}
---
+++ Tut2/Models/Studies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tut2.Models
+{
+    public class Studies
+    {
+        public string NameOfStudy { get; set; }
+        public string ModeOfStudy{ get; set; }
+    }
+}
